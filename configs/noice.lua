da873f7fdaeedd2aec549c63dbb37ddbd2cc0901
Kasper Haepers <>
2024-02-06 19:43:22 +0100

    hmmmm and better treesitter

--- configs/noice.lua
+++ configs/noice.lua
@@ -7,12 +7,12 @@ end
 noice.setup {
   lsp = {
     override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+      ["vim.lsp.util.stylize_markdown"] = false,
+      ["cmp.entry.get_documentation"] = false,
     },
     hover = {
-      enabled = true
+      enabled = false
     },
     signature = {
       enabled = false
@@ -23,6 +23,6 @@ noice.setup {
     command_palette = true, -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
+    lsp_doc_border = true, -- add a border to hover docs and signature help
   },
 }
