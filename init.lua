865a7256ff06c3983f3d003ee1a0ab0d200b41ef
Kasper Haepers <>
2024-02-23 08:23:49 +0100

    added better tabs on formatting

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+local opt = vim.opt
 -- local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
@@ -5,3 +6,9 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
